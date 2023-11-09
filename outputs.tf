cea3008270caaa61aa05a9b0f3ba70f7d9e02d98
rupeshlopes <31922904+rupeshlopes@users.noreply.github.com>
2023-11-09 21:18:33 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "service_url" {
-  value = google_cloud_run_service.run_service.status[0].url
+# Outputs
+
+output "cloud_run_url" {
+  value = element(google_cloud_run_service.default.status, 0).url
+}
+
+output "load_balancer_ip" {
+  value = google_compute_global_address.default.address
 }
