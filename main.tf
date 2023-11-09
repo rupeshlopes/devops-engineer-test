02272b3c5d8a9d2a1ece3eb00646ae31787c19b8
rupeshlopes <31922904+rupeshlopes@users.noreply.github.com>
2023-11-09 21:18:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,49 +1,107 @@
-terraform {
+# Cloud Run service and permissions
 
-  required_version = ">= 0.14"
-
-  required_providers {
-    # Cloud Run support was added on 3.3.0
-    google = {
-      source = "hashicorp/google"
-      version = "5.5.0"
-    }
-  }
-}
-
-# Enables the Cloud Run API
-resource "google_project_service" "run_api" {
-  service = "run.googleapis.com"
-
-  disable_on_destroy = true
-}
-
-# Create the Cloud Run service
-resource "google_cloud_run_service" "run_service" {
-  name = "app"
-  location = "us-central1"
+resource "google_cloud_run_service" "default" {
+  name     = var.cloud_run_service
+  location = var.region
+  project = var.project_id
 
   template {
     spec {
       containers {
+       # image = "gcr.io/cloudrun/hello"
         image = "gcr.io/google-samples/hello-app:1.0"
       }
     }
   }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-
-  # Waits for the Cloud Run API to be enabled
-  depends_on = [google_project_service.run_api]
 }
 
-# Allow unauthenticated users to invoke the service
-resource "google_cloud_run_service_iam_member" "run_all_users" {
-  service  = google_cloud_run_service.run_service.name
-  location = google_cloud_run_service.run_service.location
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
   role     = "roles/run.invoker"
   member   = "allUsers"
+}
+
+# Load Balancing resources
+
+resource "google_compute_global_address" "default" {
+  name = "${var.name}-address"
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name   = "${var.name}-fwdrule"
+
+  target = google_compute_target_https_proxy.default.id
+  port_range = "443"
+  ip_address = google_compute_global_address.default.address
+}
+
+resource "google_compute_managed_ssl_certificate" "default" {
+  provider = google-beta
+
+  name = "${var.name}-cert"
+  managed {
+    domains = ["${var.domain}"]
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name      = "${var.name}-backend"
+
+  protocol  = "HTTP"
+  port_name = "http"
+  timeout_sec = 30
+
+  backend {
+    group = google_compute_region_network_endpoint_group.cloudrun_neg.id
+  }
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "${var.name}-urlmap"
+
+  default_service = google_compute_backend_service.default.id
+}
+
+resource "google_compute_target_https_proxy" "default" {
+  name   = "${var.name}-https-proxy"
+
+  url_map          = google_compute_url_map.default.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+}
+
+resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
+  provider              = google-beta
+  name                  = "${var.name}-neg"
+  network_endpoint_type = "SERVERLESS"
+  region                = var.region
+  cloud_run {
+    service = google_cloud_run_service.default.name
+  }
+}
+
+# HTTP-to-HTTPS resources
+
+resource "google_compute_url_map" "https_redirect" {
+  name            = "${var.name}-https-redirect"
+
+  default_url_redirect {
+    https_redirect         = true
+    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"
+    strip_query            = false
+  }
+}
+
+resource "google_compute_target_http_proxy" "https_redirect" {
+  name   = "${var.name}-http-proxy"
+  url_map          = google_compute_url_map.https_redirect.id
+}
+
+resource "google_compute_global_forwarding_rule" "https_redirect" {
+  name   = "${var.name}-fwdrule-http"
+
+  target = google_compute_target_http_proxy.https_redirect.id
+  port_range = "80"
+  ip_address = google_compute_global_address.default.address
 }
